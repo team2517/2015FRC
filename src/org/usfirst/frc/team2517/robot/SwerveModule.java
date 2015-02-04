1fd2e2ac1c28457a88eca787a174fc6c8446d033
tylerscheuble <tylerscheuble@gmail.com>
2015-02-03 19:24:25 -0800

    Add SwerveModule class

---
+++ src/org/usfirst/frc/team2517/robot/SwerveModule.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team2517.robot;
+
+/* *
+ * We want the swerve module class to
+ * 1. Initialize 2 CANJaguars as parameters
+ * 2. Directly receive direction and magnitude variables through a function and move the 
+ * motorTurn jaguar until it has reached its goal
+ * 3. Look nice and have comments
+ * 
+ * You will need to basically rip code from last year's math code and convert it to java
+ * 
+ * */
+
+public class SwerveModule {
+	
+}
