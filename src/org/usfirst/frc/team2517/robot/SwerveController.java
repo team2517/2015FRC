3190a5a9c0097a528e771cfe54c8ba38c4f5a060
tylerscheuble <tylerscheuble@gmail.com>
2015-02-07 13:51:00 -0800

    Added some constructors

--- src/org/usfirst/frc/team2517/robot/SwerveController.java
+++ src/org/usfirst/frc/team2517/robot/SwerveController.java
@@ -1,15 +1,33 @@
 package org.usfirst.frc.team2517.robot;
 
 import org.usfirst.frc.team2517.robot.SwerveModule;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.CANJaguar;
+import java.lang.Math;
 
 public class SwerveController {
-	SwerveModule swerveFL = new SwerveModule(0, 0, 0); // Initialize one object for each module. Format is turnJag, moveJag, encID
-	SwerveModule swerveFR = new SwerveModule(0, 0, 0);
-	SwerveModule swerveBL = new SwerveModule(0, 0, 0);
-	SwerveModule swerveBR = new SwerveModule(0, 0, 0);
+	SwerveModule swerveFL; // Initialize one object for each module. Format is turnJag, moveJag, encID
+	SwerveModule swerveFR;
+	SwerveModule swerveBL;
+	SwerveModule swerveBR;
 	
-	public void swerve(float axisX, float axisY, float phi)
-	{
+	public SwerveController(int moveFL, int turnFL, int encFL, 
+							int moveFR, int turnFR, int encFR, 
+							int moveBL, int turnBL, int encBL, 
+							int moveBR, int turnBR, int encBR){
+		swerveFL = new SwerveModule(moveFL, turnFL, encFL);
+		swerveFR = new SwerveModule(moveFR, turnFR, encFR);
+		swerveBL = new SwerveModule(moveBL, turnBL, encBL);
+		swerveBR = new SwerveModule(moveBR, turnBR, encBR);
+	}
+	public void swerve(float axisX, float axisY, float phi){
+		if(Math.abs(axisY) > .02){ // Deadband functions
+			axisY = 0;
+		}
+		if(Math.abs(axisX) > .02){
+			axisX = 0;
+		}
+		// Makes all the left stick vectors have a magnitude of 1, rather than 1.4 in the corners.
 		
 	}
 }
