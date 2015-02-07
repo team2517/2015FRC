95bb5b2f9158033055d8f4715369dacef901590c
tylerscheuble <tylerscheuble@gmail.com>
2015-02-07 15:30:46 -0800

    Util renamed to Utils

--- src/org/usfirst/frc/team2517/robot/Utils.java
+++ src/org/usfirst/frc/team2517/robot/Utils.java
@@ -1,6 +1,6 @@
 package org.usfirst.frc.team2517.robot;
 
-public class Util {
+public class Utils {
 	public static double deadband(double input, double tolerance){ // Set number to 0 if in between tolerance and -tolerance
 		if(Math.abs(input) < tolerance){
 			return 0;
