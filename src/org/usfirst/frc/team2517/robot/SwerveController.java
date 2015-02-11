e3f4eb3115ad1c0910e9a399952746606dd67ed6
tylerscheuble <tylerscheuble@gmail.com>
2015-02-10 19:32:13 -0800

    Added mag variable in SwerveModule and added calculations for magnitude in SwerveController using pythagorean therom

--- src/org/usfirst/frc/team2517/robot/SwerveController.java
+++ src/org/usfirst/frc/team2517/robot/SwerveController.java
@@ -19,6 +19,7 @@ public class SwerveController {
 		swerveBL = new SwerveModule(moveBL, turnBL, encBL, -0.6703, 0.7421);
 		swerveBR = new SwerveModule(moveBR, turnBR, encBR, -0.7421, -0.6703);
 	}
+	
 	public void swerve(double xVector, double yVector, double phi){
 		swerveFL.x = (swerveFL.corX*phi)+xVector;
 		swerveFL.y = (swerveFL.corY*phi)+yVector; // Someday outsource these into a SwerveModule function and run 4 times instead of 8
@@ -30,6 +31,9 @@ public class SwerveController {
 		swerveBR.y = (swerveBR.corY*phi)+yVector;
 		
 		// Find magnitudes through pythagorean therom
-//		sqrt(pow(wheel[i].x, 2) + pow(wheel[i].y, 2))
+		swerveFL.mag = Math.sqrt(Math.pow(swerveFL.x,2) + Math.pow(swerveFL.y, 2));
+		swerveFR.mag = Math.sqrt(Math.pow(swerveFR.x,2) + Math.pow(swerveFR.y, 2));
+		swerveBL.mag = Math.sqrt(Math.pow(swerveBL.x,2) + Math.pow(swerveBL.y, 2));
+		swerveBR.mag = Math.sqrt(Math.pow(swerveBR.x,2) + Math.pow(swerveBR.y, 2));
 	}
 }
--- src/org/usfirst/frc/team2517/robot/SwerveModule.java
+++ src/org/usfirst/frc/team2517/robot/SwerveModule.java
@@ -21,7 +21,7 @@ public class SwerveModule {
 	public AnalogInput encoder;
 	private double turnSpeed;
 	private static double minVoltage = 0.2;
-	public double x, y, corX, corY;
+	public double x, y, corX, corY, mag;
 	public SwerveModule(int tJagID, int mTalID, int eID, double xCOR, double yCOR)
 	{
 		turnJag = new CANJaguar(tJagID);
