01e2947b795d7383c2fcceecb2b542eb62405dc1
tylerscheuble <tylerscheuble@gmail.com>
2015-02-14 15:11:13 -0800

    Undone last commit as I was wrong about the raw values being unneeded

--- src/org/usfirst/frc/team2517/robot/Robot.java
+++ src/org/usfirst/frc/team2517/robot/Robot.java
@@ -2,11 +2,7 @@
 package org.usfirst.frc.team2517.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import org.usfirst.frc.team2517.robot.*;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.CANJaguar;
-import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.DriverStation;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -22,11 +18,11 @@ public class Robot extends IterativeRobot {
      */
 	private Joystick stick;
 	private SwerveController swerveDrive;
-	private double stickX, stickY, stickPhi; // Joystick values
+	private double stickX, rawStickX, stickY, rawStickY, stickPhi; // Joystick values
 	
     public void robotInit() {
     	stick = new Joystick(0);
-    	swerveDrive = new SwerveController(30, 1, 0, 4, 0, 1);
+    	swerveDrive = new SwerveController(0, 0, 0, 0, 0, 0); // this is not okay
     }
 
     /**
@@ -40,16 +36,18 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
+    	rawStickX = Utils.deadband(stick.getRawAxis(0), 0.02); // Deadband to make sure if the value is low enough then it is 0 because when the joystick is not touched it is not always 0.
+    	rawStickY = Utils.deadband(stick.getRawAxis(1), 0.02);
+    	stickPhi = Utils.deadband(stick.getRawAxis(2), 0.02);
+    	stickX = rawStickX * Math.sqrt(1 - 0.5 * Math.pow(rawStickY, 2)); // Math equation to scale the joystick values so the difference (mag) of the vectors will be 1 instead of 1.414 (sqrt of 2)
+    	stickY = rawStickY * Math.sqrt(1 - 0.5 * Math.pow(rawStickX, 2));
+    	swerveDrive.swerve(stickX, stickY, stickPhi);
     }
     
     /**
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
-    	stickPhi = Utils.deadband(stick.getRawAxis(2), 0.02);
-    	stickX = Utils.deadband(stick.getRawAxis(0), 0.02) * Math.sqrt(1 - 0.5 * Math.pow(rawStickY, 2)); // Math equation to scale the joystick values so the difference (mag) of the vectors will be 1 instead of 1.414 (sqrt of 2)
-    	stickY = Utils.deadband(stick.getRawAxis(1), 0.02) * Math.sqrt(1 - 0.5 * Math.pow(rawStickX, 2));
-    	swerveDrive.swerve(stickX, stickY, stickPhi);
     }
     
 }
